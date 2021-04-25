f65c737fe51a1c5773f00504b45c256a7513e344
D.M <55644263+deepakmo2808@users.noreply.github.com>
2021-04-25 21:54:50 +0530

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -6,10 +6,8 @@ from selenium.webdriver.chrome.options import Options
 class SeleniumTestCase(TestCase):
     def setUp(self):
         chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('headless')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument("--remote-debugging-port=9222")
         self.driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=chrome_options)
 
 
