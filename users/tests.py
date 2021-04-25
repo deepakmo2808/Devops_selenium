fbc790db163ae22b055a9df026c12217ca5cdd89
D.M <55644263+deepakmo2808@users.noreply.github.com>
2021-04-25 22:14:24 +0530

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -7,9 +7,7 @@ class SeleniumTestCase(TestCase):
     def setUp(self):
         chrome_options = Options()
         chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--disable-extensions')
         chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
         self.driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=chrome_options)
 
